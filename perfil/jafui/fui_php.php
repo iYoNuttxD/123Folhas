db3461b38ee97f0a6643d19e2f8805df7dfeb872
Daniel Musse <127629791+iYoNuttxD@users.noreply.github.com>
2024-06-10 17:24:38 -0300

    Add files via upload

---
+++ perfil/jafui/fui_php.php
@@ -0,0 +1,19 @@
+<?php
+    include("../../connection.php"); 
+    session_start(); 
+
+    $id = $_GET["hidId"];
+    
+    try {
+        $sql = "DELETE FROM Viaja WHERE Cod_Viagem = $id";
+        if ($conn->query($sql) === TRUE) {
+            header("Location: /123folhas/perfil/jafui/index.php");
+            exit;
+        } else {
+            throw new Exception('Ocorreu um erro ao executar a operação.');
+        }
+    } catch (Exception $e) {
+        echo '<script>alert("'.$e->getMessage().'"); history.go(-1);</script>';
+        exit;
+    }
+?>
---
+++ perfil/queroir/quero_php.php
@@ -0,0 +1,19 @@
+<?php
+    include("../../connection.php"); 
+    session_start(); 
+
+    $id = $_GET["hidId"];
+    
+    try {
+        $sql = "DELETE FROM se_interessa WHERE Cod_Inteteresse = $id";
+        if ($conn->query($sql) === TRUE) {
+            header("Location: /123folhas/perfil/queroir/index.php");
+            exit;
+        } else {
+            throw new Exception('Ocorreu um erro ao executar a operação.');
+        }
+    } catch (Exception $e) {
+        echo '<script>alert("'.$e->getMessage().'"); history.go(-1);</script>';
+        exit;
+    }
+?>
